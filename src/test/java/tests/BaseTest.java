6f416d6c4f1c8e9129038375af151845551c7dc7
KurisuLim <kazuki1002003@yahoo.com>
2020-03-01 21:39:49 -0800

    add test for gird locally

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,34 +1,46 @@
 package tests;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class BaseTest {
     public WebDriver driver;
-
     @BeforeClass
-    @Parameters({"browser", "url"})
-    public void setup(String browser, String url){
+    @Parameters({"browser", "url", "nodeURL"})
+    public void setup(String browser, String url, String nodeURL) throws MalformedURLException {
 
         if(browser.equalsIgnoreCase("chrome")){
             System.setProperty("webdriver.chrome.driver", "chromedriver");
             System.setProperty("webdriver.chrome.silentOutput", "true");
             java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
-            driver = new ChromeDriver();
+            DesiredCapabilities capability = DesiredCapabilities.chrome();
+            capability.setBrowserName("chrome");
+            capability.setPlatform(Platform.ANY);
+            driver = new RemoteWebDriver(new URL(nodeURL), capability);
             driver.manage().window().fullscreen();
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         }else if(browser.equalsIgnoreCase("firefox")){
             System.setProperty("webdriver.gecko.driver", "geckodriver");
             System.setProperty("webdriver.gecko.silentOutput", "true");
             java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
-            driver = new FirefoxDriver();
+            DesiredCapabilities capability = DesiredCapabilities.firefox();
+            capability.setBrowserName("firefox");
+            capability.setPlatform(Platform.ANY);
+            driver = new RemoteWebDriver(new URL(nodeURL), capability);
             driver.manage().window().fullscreen();
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         }
